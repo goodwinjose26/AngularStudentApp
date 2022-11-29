eaf800a44df0d6c7c31268abfc69cb2d20b197af
goodwinjose26 <josegoodwin22@gmail.com>
2022-11-29 08:14:08 +0530

    readvalues

--- src/app/student-entry/student-entry.component.ts
+++ src/app/student-entry/student-entry.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./student-entry.component.css']
 })
 export class StudentEntryComponent {
+  name=""
+  rollno=""
+  adno=""
+  mob=""
+  cname=""
+  pname=""
+  pno=""
+  username=""
+  password=""
+
+  addValues =()=>
+  {
+    let data:any =
+    {
+      "name":this.name,"rollno":this.rollno,"adno":this.adno,"mob":this.mob,"cname":this.cname,"pname":this.pname,"pno":this.pno,"username":this.username,"password":this.password
+    }
+    console.log(data)
+  }
 
 }
